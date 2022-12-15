32f1e6d92d8adf623eb4c0d5c35ec134b04cbe51
ianch <thebestproian@gmail.com>
2022-12-15 00:31:28 -0600

    1.0

--- src/Day 4- Camp Cleanup.java
+++ src/Day 4- Camp Cleanup.java
@@ -1,5 +1,44 @@
-class CampCleanup {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
+class CampCleanup {
+    public static void main(String[] args) throws FileNotFoundException {
+        //System.out.println(numContains("day4_input.txt"));
+        System.out.println(numOverlap("day4_input.txt"));
+    }
+
+    private static int numContains(String fileName) throws FileNotFoundException {
+        File f = new File(fileName);
+        Scanner s = new Scanner(f);
+        int sum = 0;
+        while (s.hasNextLine()) {
+            String[] line = s.nextLine().split(",");
+            int firstStart = Integer.parseInt(line[0].substring(0, line[0].indexOf('-'))),
+                    firstEnd = Integer.parseInt(line[0].substring(line[0].indexOf('-') + 1)),
+                    secondStart = Integer.parseInt(line[1].substring(0, line[1].indexOf('-'))),
+                    secondEnd = Integer.parseInt(line[1].substring(line[1].indexOf('-') + 1));
+            if (firstStart <= secondStart && firstEnd >= secondEnd || secondStart <= firstStart && secondEnd >= firstEnd) {
+                sum++;
+            }
+        }
+        return sum;
+    }
+
+    private static int numOverlap(String fileName) throws FileNotFoundException {
+        File f = new File(fileName);
+        Scanner s = new Scanner(f);
+        int sum = 0;
+        while (s.hasNextLine()) {
+            String[] line = s.nextLine().split(",");
+            int firstStart = Integer.parseInt(line[0].substring(0, line[0].indexOf('-'))),
+                    firstEnd = Integer.parseInt(line[0].substring(line[0].indexOf('-') + 1)),
+                    secondStart = Integer.parseInt(line[1].substring(0, line[1].indexOf('-'))),
+                    secondEnd = Integer.parseInt(line[1].substring(line[1].indexOf('-') + 1));
+            if (firstStart <= secondStart && firstEnd >= secondStart || secondStart <= firstStart && secondEnd >= firstStart) {
+                sum++;
+            }
+        }
+        return sum;
     }
 }
---
+++ src/Day 5- Supply Stacks.java
@@ -0,0 +1,60 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+import java.util.Stack;
+
+class SupplyStacks {
+
+    private static ArrayList<Stack<Character>> crateStacks;
+
+    public static void main(String[] args) throws FileNotFoundException {
+        //topCrates("day5_input.txt", false);
+        topCrates("day5_input.txt", true);
+        for (Stack<Character> crateStack : crateStacks) {
+            System.out.print(crateStack.peek());
+        }
+    }
+
+    private static void topCrates(String fileName, boolean keptOrder) throws FileNotFoundException {
+        File f = new File(fileName);
+        Scanner s = new Scanner(f);
+        crateStacks = new ArrayList<>();
+        for (String line = s.nextLine(); !line.isEmpty(); line = s.nextLine()) {
+            for (int i = 0; i < line.length(); i += 4) {
+                if (line.contains("1")) {
+                    continue;
+                }
+                int index = i / 4;
+                if (crateStacks.size() <= index) {
+                    crateStacks.add(new Stack<>());
+                }
+                if (line.charAt(i + 1) != ' ') {
+                    crateStacks.get(index).push(line.charAt(i + 1));
+                }
+            }
+        }
+        for (Stack<Character> crateStack : crateStacks) {
+            Collections.reverse(crateStack);
+        }
+        // moving the boxes after initial setup
+        s.useDelimiter("\\D+");
+        while (s.hasNextInt()) {
+            int numMoved = s.nextInt(), initialStack = s.nextInt() - 1, toStack = s.nextInt() - 1;
+            if (keptOrder) {
+                Stack<Character> tempStack = new Stack<>();
+                while (numMoved-- > 0) {
+                    tempStack.push(crateStacks.get(initialStack).pop());
+                }
+                while (!tempStack.isEmpty()) {
+                    crateStacks.get(toStack).push(tempStack.pop());
+                }
+            } else {
+                while (numMoved-- > 0) {
+                    crateStacks.get(toStack).push(crateStacks.get(initialStack).pop());
+                }
+            }
+        }
+    }
+}
